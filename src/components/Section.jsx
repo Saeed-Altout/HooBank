f679fa15c20abcc0ee0b8ab825981adfe1a97de0
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-09-02 02:14:32 +0300

    edit section from py to my

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 const Section = ({ children, id }) => (
   <section
     id={id}
-    className="flex items-center justify-center w-full h-full py-12"
+    className="flex items-start justify-center w-full h-full my-12"
   >
     {children}
   </section>
